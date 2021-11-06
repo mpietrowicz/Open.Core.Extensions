5cec9d55a8ab0eae30dbf50c89f907cf271816e3
Michał Pietrowicz <michal.pietrowicz@nask.pl>
2021-11-06 15:52:33 +0100

    // WIP

---
+++ Open.Core.Extensions.Nhibernate.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Open.Core.Extensions.Nhibernate.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
